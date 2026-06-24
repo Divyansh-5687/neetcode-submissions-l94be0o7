bbb54a519b801b87551c99bddf23a791aa1c25cb
Divyansh-5687 <128798020+Divyansh-5687@users.noreply.github.com>
2026-06-24 23:00:17 +0530

    Add: palindrome-linked-list - submission-0

---
+++ Data Structures & Algorithms/palindrome-linked-list/submission-0.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        // Step 1: Find the middle using slow & fast pointers
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // slow is now at the middle
+
+        // Step 2: Reverse second half of the list
+        ListNode secondHalf = reverse(slow);
+
+        // Step 3: Compare both halves
+        ListNode left = head;
+        ListNode right = secondHalf;
+
+        while (right != null) {          // second half is shorter or equal
+            if (left.val != right.val) return false;
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;  // new head of reversed list
+    }
+}
